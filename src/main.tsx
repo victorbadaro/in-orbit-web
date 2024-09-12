0c2f4dcfee3f3e9dbd36257aba1aff8f3e52dc1a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-12 18:51:39 -0300

    feat: change summary request to use tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import { App } from './app.tsx';
 
 import './index.css';
 
 const container = document.getElementById('root');
+const queryClient = new QueryClient();
 
-createRoot(container!).render(<App />);
+createRoot(container!).render(
+	<QueryClientProvider client={queryClient}>
+		<App />
+	</QueryClientProvider>
+);
