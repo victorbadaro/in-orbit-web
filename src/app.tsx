e839525eb895e5fe3144c34fca5d90fad5cf194a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-09-12 00:04:30 -0300

    feat: separate dialog and empty goals page in different files

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,12 @@
-import { Plus } from 'lucide-react';
-
-import letsStart from './assets/lets-start-illustration.svg';
-import logo from './assets/logo-in-orbit.svg';
-import { Button } from './components/ui/button';
+import { CreateGoal } from './components/create-goal';
+import { EmptyGoals } from './components/empty-goals';
+import { Dialog } from './components/ui/dialog';
 
 export function App() {
 	return (
-		<div className="h-screen flex flex-col items-center justify-center gap-8">
-			<img src={logo} alt="in.orbit" />
-			<img src={letsStart} alt="in.orbit" />
-			<p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-				Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-			</p>
-
-			<Button>
-				<Plus className="size-4" />
-				Cadastrar meta
-			</Button>
-		</div>
+		<Dialog>
+			<EmptyGoals />
+			<CreateGoal />
+		</Dialog>
 	);
 }
